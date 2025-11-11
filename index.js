75b612a933a7594d5b5f3f13aa66a67e2a053111
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-11 16:10:04 +0700

    membuat koenksi ke data base

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const crypto = require('crypto');
+const path = require('path');
+const mysql = require('mysql2');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'apikey_db',
+  port: 3306
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Gagal terhubung ke database:', err);
+  } else {
+    console.log('✅ Koneksi ke MySQL berhasil!');
+  }
+});
+
+function generateApiKey() {
+  const randomBytes = crypto.randomBytes(8).toString('hex');
+  return `sk-sm-v1-${randomBytes}`;
+}
+
